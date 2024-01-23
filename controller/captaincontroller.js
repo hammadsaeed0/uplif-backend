544aa4ec3eb841cdb8867407db00e30b4b4d7c81
Coderpro <101320063+mubeenayub1@users.noreply.github.com>
2024-01-23 17:26:57 +0500

    update

--- controller/captaincontroller.js
+++ controller/captaincontroller.js
@@ -6,20 +6,31 @@ export const register = catchAsyncError(async (req, res, next) => {
   const phoneNumber = data?.phoneNumber;
   const existingCaptain = await Captain.findOne({ phoneNumber });
   if (existingCaptain) {
-    res.status(201).json({
-      success: "success",
-      status:201,
-      message: "User login successfully",
-      data: existingUser,
-    });
+    res.status(400).json({ message: "This phone number already exist", status: "fail" });
   } else {
     const newCaptain = await Captain.create(data);
 
     res.status(200).json({
-      success: "success",
-      status:200,
+      status: "success",
       message: "Captain registered successfully",
       data: newCaptain,
     });
+  }
+});
+
+export const login = catchAsyncError(async (req, res, next) => {
+  try {
+    const { phoneNumber} = req.body;
+    const existingUser = await Captain.findOne({ phoneNumber });
+    if (!existingUser) {
+      res.status(400).json({ message: "phone number not exist", status: "fail" });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "User login successfully",
+      data: existingUser,
+    });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
   }
 });
